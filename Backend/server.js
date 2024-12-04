5817a7606bdd07d4c45acd8e87ea5ce9148ecf8b
Anand Dubey <124145838+nytewlker@users.noreply.github.com>
2024-12-04 11:39:06 +0530

    hh

--- Backend/server.js
+++ Backend/server.js
@@ -32,5 +32,7 @@ app.use('/api/application', require('./routes/application'));
 app.use('/api/contact', require('./routes/contact'));
 app.use('/api/subscribe', require('./routes/suscribe'));
 
-// Export the app for serverless functions
-module.exports = app;
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
