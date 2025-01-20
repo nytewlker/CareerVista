b14dbd8318f4b14c28188db4a360aab890fe4184
Anand Dubey <ananddubey.connect@gmail.com>
2025-01-20 10:44:05 +0530

    Update EmployeeProfile.jsx

--- Frontend/src/components/pages/Employee/EmployeeProfile.jsx
+++ Frontend/src/components/pages/Employee/EmployeeProfile.jsx
@@ -86,7 +86,7 @@ const EmployeeProfile = () => {
         value={value}
         onChange={onChange}
         required={required}
-        className="w-full px-4 py-2 border tex-black rounded-lg focus:outline-none focus:ring-2 focus:ring-yellow-500"
+        className="w-full px-4 py-2 border tex-black   rounded-lg focus:outline-none focus:ring-2 focus:ring-yellow-500"
       />
     </div>
   );
